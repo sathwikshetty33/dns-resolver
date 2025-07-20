05c71e970754a8065177378a6086841739b7cd67
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2025-07-20 06:07:29 +0000

    added func to query root servers and parse resposne

--- dns/resolver.go
+++ dns/resolver.go
@@ -1,7 +1,9 @@
 package dns
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
 	"net"
 
 	"golang.org/x/net/dns/dnsmessage"
@@ -14,5 +16,44 @@ func handlePacket(pc net.PacketConn, addr net.Addr, buf []byte) error {
 }
 
 func outgoingDnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Parser, *dnsmessage.Header, error) {
-	return nil, nil, nil
+	id := uint16(rand.Intn(65536))
+	// Query
+	message := dnsmessage.Message{
+		Header: dnsmessage.Header{
+			ID:       id,                   // random num to match questions with responses
+			Response: false,                //tells the server it is query not response
+			OpCode:   dnsmessage.OpCode(0), //standard DNS query
+		},
+		Questions: []dnsmessage.Question{question},
+	}
+	buf, err := message.Pack() // Packing the message to bytes so that it can be transmitted over UDP
+	if err != nil {
+		return nil, nil, err
+	}
+	var conn net.Conn
+	for _, server := range servers {
+		conn, err = net.Dial("udp", server.String()+":53") //Establishing a UDP connection with one of the root servers
+		if err == nil {
+			break // Break if connection with any one of them is established
+		}
+	}
+	if conn == nil {
+		return nil, nil, fmt.Errorf("could not connect to DNS server")
+	}
+	_, err = conn.Write(buf) // Sends the query to the connect root server
+	ans := make([]byte, 512)
+	n, err := bufio.NewReader(conn).Read(ans) //Reads up to 512 bytes from the response (the traditional DNS UDP limit). and store in ans
+	if err != nil {
+		return nil, nil, err
+	}
+	err = conn.Close()
+	if err != nil {
+		return nil, nil, err
+	}
+	var p dnsmessage.Parser
+	headers, err := p.Start(ans[:n])
+	if err != nil {
+		return nil, nil, fmt.Errorf("parser start errords:%s", err)
+	}
+	return &p, &headers, nil
 }
