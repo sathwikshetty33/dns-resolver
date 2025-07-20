c9eb640b3de7175ee4231021c876c444bd668fdf
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2025-07-20 14:11:57 +0000

    added cname support

--- dns/resolver.go
+++ dns/resolver.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net"
 	"strings"
+	"time"
 
 	"golang.org/x/net/dns/dnsmessage"
 )
@@ -15,6 +16,20 @@ const ROOT_SERVERS = "198.41.0.4,199.9.14.201,192.33.4.12,199.7.91.13,192.203.23
 func HandlePacket(pc net.PacketConn, addr net.Addr, buf []byte) {
 	if err := handlePacket(pc, addr, buf); err != nil {
 		fmt.Printf("handlePacket error: %s\n", err)
+		// Send error response back to client
+		sendErrorResponse(pc, addr, dnsmessage.RCodeServerFailure)
+	}
+}
+
+func sendErrorResponse(pc net.PacketConn, addr net.Addr, rcode dnsmessage.RCode) {
+	errorMsg := dnsmessage.Message{
+		Header: dnsmessage.Header{
+			Response: true,
+			RCode:    rcode,
+		},
+	}
+	if buf, err := errorMsg.Pack(); err == nil {
+		pc.WriteTo(buf, addr)
 	}
 }
 
@@ -28,187 +43,256 @@ func handlePacket(pc net.PacketConn, addr net.Addr, buf []byte) error {
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Question: %s\n", question.Name.String())
+
 	res, err := dnsQuery(getRootServers(), question)
 	if err != nil {
 		return err
 	}
+
 	res.Header.ID = header.ID
 	responseBuffer, err := res.Pack()
 	if err != nil {
 		return err
 	}
+
 	_, err = pc.WriteTo(responseBuffer, addr)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
+
 func dnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Message, error) {
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 10; i++ { // Increased iteration limit
+		fmt.Printf("Iteration %d: Querying %s\n", i+1, question.Name.String())
+
 		dnsAnswer, header, err := outgoingDnsQuery(servers, question)
 		if err != nil {
 			fmt.Printf("outgoingDnsQuery error: %s\n", err)
 			return nil, err
 		}
+
+		// Check for CNAME records first
 		parsedAnswers, err := dnsAnswer.AllAnswers()
 		if err != nil {
 			return nil, err
 		}
-		if header.Authoritative {
-			fmt.Printf("The Ip is %s", parsedAnswers[0].GoString())
+
+		for _, ans := range parsedAnswers {
+			if ans.Header.Type == dnsmessage.TypeCNAME {
+				cname := ans.Body.(*dnsmessage.CNAMEResource).CNAME.String()
+				fmt.Printf("Following CNAME: %s -> %s\n", question.Name.String(), cname)
+				return dnsQuery(getRootServers(), dnsmessage.Question{
+					Name:  dnsmessage.MustNewName(cname),
+					Type:  dnsmessage.TypeA,
+					Class: dnsmessage.ClassINET,
+				})
+			}
+		}
+
+		// If we have answers and it's authoritative, we're done
+		if header.Authoritative && len(parsedAnswers) > 0 {
+			fmt.Printf("Authoritative answer found with %d records\n", len(parsedAnswers))
 			return &dnsmessage.Message{
-				Header:    dnsmessage.Header{Response: true},
+				Header:    dnsmessage.Header{Response: true, Authoritative: true},
 				Questions: []dnsmessage.Question{question},
 				Answers:   parsedAnswers,
 			}, nil
 		}
+
+		// Get authority records (NS records)
 		authorities, err := dnsAnswer.AllAuthorities()
 		if err != nil {
 			return nil, err
 		}
+
 		if len(authorities) == 0 {
 			return &dnsmessage.Message{
 				Header: dnsmessage.Header{
-					RCode: dnsmessage.RCodeNameError,
+					RCode:    dnsmessage.RCodeNameError,
+					Response: true,
 				},
+				Questions: []dnsmessage.Question{question},
 			}, nil
 		}
-		nameServers := make([]string, len(authorities))
-		for k, authority := range authorities {
+
+		// Extract nameservers from authority section
+		var nameServers []string
+		for _, authority := range authorities {
 			if authority.Header.Type == dnsmessage.TypeNS {
 				if ns, ok := authority.Body.(*dnsmessage.NSResource); ok {
-					nameServers[k] = ns.NS.String()
+					nameServers = append(nameServers, ns.NS.String())
 				}
 			}
 		}
+
+		if len(nameServers) == 0 {
+			continue
+		}
+
+		fmt.Printf("Found %d nameservers: %v\n", len(nameServers), nameServers)
+
+		// Check for glue records in additional section
 		additionals, err := dnsAnswer.AllAdditionals()
 		if err != nil {
 			return nil, err
 		}
-		newResolverServers := false
+
 		var newServers []net.IP
+		glueFound := false
+
 		for _, additional := range additionals {
 			if additional.Header.Type == dnsmessage.TypeA {
+				nsName := additional.Header.Name.String()
 				for _, nameServer := range nameServers {
-					if additional.Header.Name.String() == nameServer {
-						newResolverServers = true
-						newServers = append(newServers, additional.Body.(*dnsmessage.AResource).A[:])
+					if nsName == nameServer {
+						ip := additional.Body.(*dnsmessage.AResource).A[:]
+						newServers = append(newServers, ip)
+						glueFound = true
+						fmt.Printf("Found glue record: %s -> %s\n", nsName, net.IP(ip).String())
 					}
 				}
-				if len(newServers) > 0 {
-					return dnsQuery(newServers, question)
-				}
-
-				if !newResolverServers {
-					for _, nameServer := range nameServers {
-						if !newResolverServers {
-							res, err := dnsQuery(getRootServers(), dnsmessage.Question{Name: dnsmessage.MustNewName(nameServer), Type: dnsmessage.TypeA, Class: dnsmessage.ClassINET})
-
-							if err != nil {
-								fmt.Println("lookup failed for " + nameServer)
-							} else {
-								newResolverServers = true
-								for _, answer := range res.Answers {
-									if answer.Header.Type == dnsmessage.TypeA {
-										newServers = append(newServers, answer.Body.(*dnsmessage.AResource).A[:])
-									}
-								}
-							}
-						}
-					}
-				}
-				fmt.Println("Ips's Found ", newServers)
 			}
 		}
+
+		// If we found glue records, use them for next query
+		if glueFound && len(newServers) > 0 {
+			servers = newServers
+			continue
+		}
+
+		// No glue records found, need to resolve nameservers
+		fmt.Printf("No glue records found, resolving nameservers...\n")
+		newServers = nil
+
+		for _, nameServer := range nameServers {
+			fmt.Printf("Resolving nameserver: %s\n", nameServer)
+			res, err := dnsQuery(getRootServers(), dnsmessage.Question{
+				Name:  dnsmessage.MustNewName(nameServer),
+				Type:  dnsmessage.TypeA,
+				Class: dnsmessage.ClassINET,
+			})
+
+			if err != nil {
+				fmt.Printf("Failed to resolve nameserver %s: %s\n", nameServer, err)
+				continue
+			}
+
+			// Extract IP addresses from the response
+			for _, answer := range res.Answers {
+				if answer.Header.Type == dnsmessage.TypeA {
+					ip := answer.Body.(*dnsmessage.AResource).A[:]
+					newServers = append(newServers, ip)
+					fmt.Printf("Resolved %s -> %s\n", nameServer, net.IP(ip).String())
+				}
+			}
+
+			// If we got at least one IP, we can proceed
+			if len(newServers) > 0 {
+				break
+			}
+		}
+
+		if len(newServers) == 0 {
+			return &dnsmessage.Message{
+				Header: dnsmessage.Header{
+					RCode:    dnsmessage.RCodeServerFailure,
+					Response: true,
+				},
+				Questions: []dnsmessage.Question{question},
+			}, nil
+		}
+
+		servers = newServers
 	}
+
+	// If we've exceeded max iterations, return server failure
 	return &dnsmessage.Message{
 		Header: dnsmessage.Header{
-			RCode:    dnsmessage.RCodeNameError,
+			RCode:    dnsmessage.RCodeServerFailure,
 			Response: true,
 		},
 		Questions: []dnsmessage.Question{question},
 	}, nil
-
 }
 
 func getRootServers() []net.IP {
-	rootServers := strings.Split(ROOT_SERVERS, ",")
-	if len(rootServers) == 0 {
-		panic("No root servers found")
-	}
 	var rootServersSlice []net.IP
 	for _, rootServer := range strings.Split(ROOT_SERVERS, ",") {
 		if ip := net.ParseIP(rootServer); ip != nil {
 			rootServersSlice = append(rootServersSlice, ip)
 		}
 	}
-
 	return rootServersSlice
 }
+
 func outgoingDnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Parser, *dnsmessage.Header, error) {
 	id := uint16(rand.Intn(65536))
-	// Query
 	message := dnsmessage.Message{
 		Header: dnsmessage.Header{
-			ID:       id,                   // random num to match questions with responses
-			Response: false,                //tells the server it is query not response
-			OpCode:   dnsmessage.OpCode(0), //standard DNS query
+			ID:               id,
+			Response:         false,
+			OpCode:           dnsmessage.OpCode(0),
+			RecursionDesired: false, // We're doing iterative resolution
 		},
 		Questions: []dnsmessage.Question{question},
 	}
-	fmt.Printf("Querying %s", question.Name.String())
-	buf, err := message.Pack() // Packing the message to bytes so that it can be transmitted over UDP
+
+	buf, err := message.Pack()
 	if err != nil {
 		return nil, nil, err
 	}
-	var conn net.Conn
+
+	var lastErr error
 	for _, server := range servers {
-		conn, err = net.Dial("udp", server.String()+":53") //Establishing a UDP connection with one of the root servers
-		if err == nil {
-			break // Break if connection with any one of them is established
+		fmt.Printf("Trying server: %s\n", server.String())
+
+		conn, err := net.DialTimeout("udp", server.String()+":53", 5*time.Second)
+		if err != nil {
+			lastErr = err
+			continue
 		}
+
+		// Set read/write timeouts
+		conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+		_, err = conn.Write(buf)
+		if err != nil {
+			conn.Close()
+			lastErr = err
+			continue
+		}
+
+		ans := make([]byte, 512)
+		n, err := bufio.NewReader(conn).Read(ans)
+		conn.Close()
+
+		if err != nil {
+			lastErr = err
+			continue
+		}
+
+		var p dnsmessage.Parser
+		headers, err := p.Start(ans[:n])
+		if err != nil {
+			lastErr = fmt.Errorf("parser start error: %s", err)
+			continue
+		}
+
+		// Verify this is a response to our query
+		if headers.ID != id {
+			lastErr = fmt.Errorf("ID mismatch: expected %d, got %d", id, headers.ID)
+			continue
+		}
+
+		// Skip questions section
+		err = p.SkipAllQuestions()
+		if err != nil {
+			lastErr = fmt.Errorf("parser questions skip error: %s", err)
+			continue
+		}
+
+		return &p, &headers, nil
 	}
-	if conn == nil {
-		return nil, nil, fmt.Errorf("could not connect to DNS server")
-	}
-	_, err = conn.Write(buf) // Sends the query to the connect root server
-	ans := make([]byte, 512)
-	n, err := bufio.NewReader(conn).Read(ans) //Reads up to 512 bytes from the response (the traditional DNS UDP limit). and store in ans
-	if err != nil {
-		return nil, nil, err
-	}
-	err = conn.Close() //Closes the UDP connection after reading the response.
-	if err != nil {
-		return nil, nil, err
-	}
-	var p dnsmessage.Parser
-	headers, err := p.Start(ans[:n]) // initializes the parser and reads the DNS header and other internal state.
-	if err != nil {
-		return nil, nil, fmt.Errorf("parser start errords:%s", err)
-	}
-	questions, err := p.AllQuestions()
-	if err != nil {
-		return nil, nil, fmt.Errorf("parser questions errors:%s", err)
-	}
-	if len(message.Questions) != len(questions) {
-		return nil, nil, fmt.Errorf("parser questions length errords:%d", len(questions))
-	}
-	err = p.SkipAllQuestions() // Moves the parsers past questions to next section
-	if err != nil {
-		return nil, nil, fmt.Errorf("parser questions skip errords:%s", err)
-	}
-	return &p, &headers, nil // headers gives you the metadata: response ID, response code, flags (like RecursionAvailable, Authoritative, etc.).
+
+	return nil, nil, fmt.Errorf("all servers failed, last error: %v", lastErr)
 }
-
-/* DNS Message Structure Reminder
-A DNS message is composed of these sections in this exact order:
-
-Header
-
-Questions (what you're asking)
-
-Answers (the direct answer)
-
-Authority (where to go next)
-
-Additional (helpful info) */
